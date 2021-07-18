09bc5e4dd579fbb5f0087af142c272ad63774538
ivGomes <ivan.kyogomes@gmail.com>
2021-07-17 22:10:45 -0300

    função de adicionar os balões quando a página é carregada

--- js/gameplay.js
+++ js/gameplay.js
@@ -2,6 +2,8 @@
 const _TIMER_EASY = 120;
 const _TIMER_NORMAL = 60;
 const _TIMER_HARD = 30;
+//...quantidade de baloes
+const _BALLOONS = 50;
 //...pega o id da dificuldade
 const _GAME_DIFICULTY = parseInt((window.location.search).replace('?', ''));
 //...pega local dos timers
@@ -25,5 +27,19 @@ window.onload = function (){
         _TIMER_INDICATOR[1].innerHTML = _TIMER_HARD;
 
     }
+
+    for (let i = 0; i < _BALLOONS; i++) {
+        const _EL_BALLONS = document.createElement('li');
+        const _EL_IMG_BALLONS = document.createElement('img');
+        const _EL_TARGET = document.getElementsByClassName('balloonList');
+        
+        _EL_IMG_BALLONS.src = "../imagens/balao_azul_pequeno.png";
+        _EL_BALLONS.appendChild(_EL_IMG_BALLONS);
+
+        console.log(_EL_BALLONS);
+        _EL_TARGET[0].appendChild(_EL_BALLONS);
+    }
+
+    
 }
 
